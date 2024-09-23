b75774bb4ea007fb479d5ea721691e8db12982e2
Kitaaaaaaa <bac1772004@gmail.com>
2024-09-23 22:30:34 +0700

    update GUI

--- GUI/ConvertCF/app/src/main/java/com/example/convertcf/MainActivity.java
+++ GUI/ConvertCF/app/src/main/java/com/example/convertcf/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.convertcf;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.*;
 
 import androidx.activity.EdgeToEdge;
@@ -17,9 +18,40 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-
+        initControl();
     }
     private void initControl(){
-        btCF
+        btCF = findViewById(R.id.btCF);
+        btFC = findViewById(R.id.btFC);
+        et1 = findViewById(R.id.tb1);
+        et2 = findViewById(R.id.tb2);
+        btClr = findViewById(R.id.btClear);
+        btCF.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!et1.getText().toString().isEmpty())
+                {
+                    Double cf = Double.parseDouble(et1.getText().toString());
+                    et2.setText(Double.toString(cf*9/5+32));
+                }
+            }
+        });
+        btFC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!et2.getText().toString().isEmpty())
+                {
+                    Double fc = Double.parseDouble(et2.getText().toString());
+                    et1.setText(Double.toString((fc-32)*5/9));
+                }
+            }
+        });
+        btClr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                et1.setText("");
+                et2.setText("");
+            }
+        });
     }
 }
